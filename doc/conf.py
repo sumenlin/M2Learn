3f1b5d842662fd961356b578b4be0950f28afed3
Suwen Lin <30698452+sumenlin@users.noreply.github.com>
2019-02-18 01:42:06 -0500

    Add files via upload

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import m2learn
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
