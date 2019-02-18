82a13cee4a37cc15430c0a292d2d492bc68887b9
Suwen Lin <30698452+sumenlin@users.noreply.github.com>
2019-02-18 04:51:47 -0500

    Update __init__.py

--- m2learn/preprocessing/__init__.py
+++ m2learn/preprocessing/__init__.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# """
-# M2Learn: An python library for multi-modal data learning.
-# - Homepage: https://github.com/sumenlin/M2Learn/
-# - Author: Suwen Lin and contributors
-# - License: New BSD
-# - Date: February 2019
-# """
+
 from __future__ import division, print_function  # Python 2 compatibility
 
 __author__ = "Suwen"
