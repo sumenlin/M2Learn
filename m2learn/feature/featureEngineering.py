d5771f89e09bb670a54741c43a0cb559f8a0d340
Suwen Lin <30698452+sumenlin@users.noreply.github.com>
2019-02-20 22:40:57 -0500

    Add files via upload

--- m2learn/feature/featureEngineering.py
+++ m2learn/feature/featureEngineering.py
@@ -33,7 +33,37 @@ def featureExtraction(data,identification,featureList=None,statis = ['mean','std
     :returns: data after adding the extracted statistics
     :rtype: data frame
     """
-    
+    nx = [identification]
+    if featureList==None:
+        featureList = list(set(data.columns.tolist())-set([identification]))
+    for x in featureList:
+        if 'mean' in statis:
+            df = data.groupby(identification)[x].mean().reset_index()
+            df = df.rename(index=str,columns={x:x+"_mean"})
+            data = pd.merge(df,data,on=identification,how='right')
+            nx.append(x+"_mean")
+        if 'std' in statis:
+            df = data.groupby(identification)[x].std().reset_index()
+            df = df.rename(index=str,columns={x:x+"_std"})
+            data = pd.merge(df,data,on=identification,how='right')
+            nx.append(x+"_std")
+        if 'median' in statis:
+            df = data.groupby(identification)[x].median().reset_index()
+            df = df.rename(index=str,columns={x:x+"_median"})
+            data = pd.merge(df,data,on=identification,how='right')
+            nx.append(x+"_median")
+        if 'max' in statis:
+            df = data.groupby(identification)[x].max().reset_index()
+            df = df.rename(index=str,columns={x:x+"_max"})
+            data = pd.merge(df,data,on=identification,how='right')
+            nx.append(x+"_max")
+        if 'min' in statis:
+            df = data.groupby(identification)[x].min().reset_index()
+            df = df.rename(index=str,columns={x:x+"_min"})
+            data = pd.merge(df,data,on=identification,how='right')
+            nx.append(x+"_min")
+    data = data.ix[:,nx]
+    return data
 
 #PCA 
 def featurePCA(data_train,data_test,pca_number = 3,random_state = 40):
@@ -50,7 +80,10 @@ def featurePCA(data_train,data_test,pca_number = 3,random_state = 40):
     :returns: data after PCA analysis
     :rtype: data frame
     """
-    
+    pca = PCA(n_components=pca_number, random_state=random_state).fit(data_train)
+    data_train_pca = pca.transform(data_train)
+    data_test_pca = pca.transform(data_test)
+    return data_train_pca,data_test_pca
 
 
 #feature selection on training data
@@ -72,5 +105,86 @@ def selectFeature(data,target, kind = 'all',co_linear = False,N = None):
     :returns: list of potential selected feature lists
     :rtype: list of list
     """
-    
+    M = int((len(data.index))**(0.5))
+    if N==None:
+        N = min(len(data.columns.tolist()),M)
+        mN = min(5,len(data.columns.tolist()))
+        LN = range(mN,N+1,5)
+    elif type(N)==int:
+        LN = [N]
+    elif type(N)==list:
+        LN = N
+    y = target
+
+
+    Xheads = data.columns.tolist()
+    Xheads_list = []
+    df = pd.DataFrame()
+    df['Xheads'] = Xheads
+    if kind == 'correlation' or kind == 'all':
+    # correlation selection
+        corrs = []
+        d1 = y.tolist()
+        for xhead in Xheads:
+            # print xhead
+            # d1 = data[yhead].tolist()
+            d2 = data[xhead].tolist()
+            corr = pearsonr(d1,d2)
+            corrs.append(corr[0])
+            # corrs.append(abs(corr[0]))
+        df['correlation'] = corrs
+        df = df.sort_values(by='correlation', ascending=False).reset_index(drop=True)
+        if not co_linear:
+            for n in LN:
+                Xheads_new = df.Xheads.tolist()[:n]
+                Xheads_list.append(Xheads_new)
+        else:
+            tmp_x = []
+            tmp = 0
+            Xheads_new = df.Xheads.tolist()
+            while tmp<len(Xheads_new) and len(tmp_x)<LN[-1]:
+                d1 = data[Xheads_new[tmp]].tolist()
+                for tx in tmp_x:
+                    d2 = data[tx].tolist()
+                    if pearsonr(d1,d2)[0]>0.5:
+                        break
+                else:
+                    tmp_x.append(Xheads_new[tmp])
+                    if len(tmp_x) in LN:
+                        Xheads_list.append(tmp_x)
+                tmp += 1
+
+    #linear
+    if kind == 'linear' or kind == 'all':
+        X = data
+        Y = y
+        lr = LinearRegression()  
+        lr.fit(X, Y)
+        df['lr_coeff'] = lr.coef_.tolist()
+        df = df.sort_values(by='lr_coeff', ascending=False).reset_index(drop=True)
+        # for n in LN:
+        #     Xheads_new = df.Xheads.tolist()[:n]
+        #     Xheads_list.append(Xheads_new)
+
+        if not co_linear:
+            for n in LN:
+                Xheads_new = df.Xheads.tolist()[:n]
+                Xheads_list.append(Xheads_new)
+        else:
+            tmp_x = []
+            tmp = 0
+            Xheads_new = df.Xheads.tolist()
+            while tmp<len(Xheads_new) and len(tmp_x)<LN[-1]:
+                d1 = data[Xheads_new[tmp]].tolist()
+                for tx in tmp_x:
+                    d2 = data[tx].tolist()
+                    if pearsonr(d1,d2)[0]>0.5:
+                        break
+                else:
+                    tmp_x.append(Xheads_new[tmp])
+                    if len(tmp_x) in LN:
+                        Xheads_list.append(tmp_x)
+                tmp += 1
+
+    return Xheads_list
 
